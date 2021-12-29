bc144ab49981c9c2fe54cd8e11b39feac13b798c
Prathik Reddy <tvkp333555@gmail.com>
2021-12-29 23:35:42 +0530

    Dynamic Programming and Greedy day1

---
+++ Level1/03.Dynamic Programming/01.Dynamic Programming and Greedy/01.Fibonacci-dp.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) throws Exception {
+    // write your code here
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] dp = new int[n+1];
+    System.out.println(fibo(n,dp));
+ }
+public static int fibo(int n , int[] dp){
+    if(n==0||n==1){
+        return n;
+    }
+    if(n<0){
+        return 0;
+    }
+    if(dp[n]!=0){
+        return dp[n];
+    }
+
+    int fibo1 = fibo(n-1,dp);
+    int fibo2 = fibo(n-2,dp);
+    int fibo = fibo1 + fibo2;
+
+    dp[n] = fibo;
+    return dp[n];
+
+}
+}
