ba3f2dbc9a2a097525f37d76c7f85a427e575822
Prathik Reddy <tvkp333555@gmail.com>
2022-01-16 22:29:35 +0530

    dp rev day5

--- Level1/03.Dynamic Programming/01.Dynamic Programming and Greedy/27.TilingWith2x1Tiles.java
+++ Level1/03.Dynamic Programming/01.Dynamic Programming and Greedy/27.TilingWith2x1Tiles.java
@@ -5,6 +5,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         Scanner scn = new Scanner(System.in);
+        
         int n = scn.nextInt();
 
         int[] dp = new int[n+1];
